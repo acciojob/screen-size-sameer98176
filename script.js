efbb71fd72fada67e06001707e752e3bec0029b2
Sameer Baghel <sameerbaghel0001@gmail.com>
2026-04-24 11:17:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+//your JS code here. If required
+function updateWindowSize() {
+    document.getElementById("size").innerText =
+        "Width: " + window.innerWidth +
+        " and Height: " + window.innerHeight;
+}
+updateWindowSize();
+window.addEventListener("resize", updateWindowSize);
